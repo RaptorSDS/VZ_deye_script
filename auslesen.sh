08bc2fc0041306a8d7de18dbe989a714c708ea4b
RaptorSDS <raptor.o2@hotmail.de>
2023-05-27 16:12:37 +0200

    Update auslesen.sh
    extra Total with filter and check non-Zero

--- auslesen.sh
+++ auslesen.sh
@@ -1,40 +1,47 @@
 #!/bin/bash
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-# set -e stop the script when Invert not reachable
+
 set -e
 
-# Script: auslesen.sh
-# Author: Tobias Baumann aka RaptorSDS
-# License: MIT
-
-# additional need script from dr-ni
-# https://github.com/dr-ni/mi600
-
-
-
-
-#Area for Login Data
-host_pv="192.xxx.xxx.xxx"
+host_pv="192.168.2.28"
 user="admin"
 password="admin"
-UUID1="7680efc0-xxx-xxxx-xxxx-xxxxxxx"
-#UUID2=""
-host_db="192.xxx.xxx.xxx"
+UUID1="7680efc0-aabe-11ed-8cfb-f1a7e776cc49"
+UUID2="d69e8d40-fbf0-11ed-bd53-3b795ba98c6f"
+host_db="192.168.2.51"
+TOTAL=""
+TOTAL_NUM=""
 ACTUAL=""
 ACTUAL_NUM=""
 
-#area for request data
-#TOTAL=$(/usr/local/bin/mi600 $host_pv $user $password webdata_total_e)
-#DAY=$(/usr/local/bin/mi600 $host_pv $user $password webdata_today_e)
-ACTUAL=$(/usr/local/bin/mi600 $host_pv $user $password webdata_now_p)
 
-#remove empty space
+#request value
+ACTUAL=$(/usr/local/bin/mi600 $host_pv $user $password webdata_now_p)
+#remove Zero in Front
 ACTUAL_NUM=$(($ACTUAL))
 
-#Senden to database
-wget -O - -q "http://"$host_db"/middleware/data/"$UUID1".json?operation=add&value="$ACTUAL_NUM""
-#wget -O - -q "http://"$host_db"/middleware/data/"$UUID2".json?operation=add&value"$DAY""
 
-#show value for debug
+#send database
+wget -O - -q "http://"$host_db"/middleware/data/"$UUID1".json?operation=add&value="$ACTUAL_NUM""
+
+
+
+#request value TOTAL
+TOTAL=$(/usr/local/bin/mi600 $host_pv $user $password webdata_total_e)
+#Remove end SPACE-Char
+TOTAL_NUM=$(echo $TOTAL | sed 's/[[:space:]]*$//')
+
+#check if non ZERO
+if [ $TOTAL_NUM != 0 ]; then
+#Send to DB
+  wget -O - -q "http://"$host_db"/middleware/data/"$UUID2".json?operation=add&value="$TOTAL_NUM""
+fi
+
+
+
+
+#DAY=$(/usr/local/bin/mi600 $host_pv $user $password webdata_today_e)
+
 #echo 'Today:'$DAY
 #echo 'ACTUAL:'$ACTUAL_NUM
+#echo 'TOTAL:'$TOTAL_NUM
