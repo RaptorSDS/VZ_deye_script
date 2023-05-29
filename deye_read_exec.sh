d0f875edf98b2376a71e52e3fbe06a3918476a24
RaptorSDS <raptor.o2@hotmail.de>
2023-05-29 14:01:32 +0200

    Create deye_read_exec.sh

---
+++ deye_read_exec.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+
+#set -e / set -u stop scripts if something is wrong
+set -e
+set -u
+
+# Function to display script usage
+usage() {
+  echo "Usage: $0 <hostname> <reading1> <reading2> ..."
+  exit 1
+}
+
+# Validate required arguments
+if [ "$#" -lt 2 ]; then
+  usage
+fi
+
+# Extract hostname
+HOSTNAME="$1"
+shift
+
+# Extract readings
+READINGS="$*"
+
+user="admin"
+password="admin"
+
+# Request values based on input readings
+for READING in $READINGS; do
+  if [ "$READING" == "ACTUAL" ]; then
+    ACTUAL=$(/usr/local/bin/mi600 "$HOSTNAME" "$user" "$password" webdata_now_p)
+    ACTUAL_NUM=$(($ACTUAL))
+    OUTPUT="$ACTUAL_NUM"
+    printf "%s = %s\n" "$READING" "$OUTPUT"
+  elif [ "$READING" == "TOTAL" ]; then
+    TOTAL=$(/usr/local/bin/mi600 "$HOSTNAME" "$user" "$password" webdata_total_e)
+    TOTAL_NUM=$(echo "$TOTAL" | sed 's/[[:space:]]*$//')
+    if [ $TOTAL_NUM == "0.0" ]; then
+      OUTPUT=""
+    else
+      OUTPUT="$TOTAL_NUM"
+    fi
+    printf "%s = %s\n" "$READING" "$OUTPUT"
+  elif [ "$READING" == "DAY" ]; then
+    DAY=$(/usr/local/bin/mi600 "$HOSTNAME" "$user" "$password" webdata_today_e)
+    OUTPUT="$DAY"
+    printf "%s = %s\n" "$READING" "$OUTPUT"
+  else
+    echo "Invalid reading: $READING"
+  fi
+done
